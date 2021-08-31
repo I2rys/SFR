aee37aa5c2de018cf9a19f10686ee4546f5dcac5
I2rys <i2rys@protonmail.com>
2021-08-30 18:02:30 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+//Dependencies
+const Secure_RM = require("secure-rm")
+const I2rys = require("./utils/i2rys")
+const Fs = require("fs")
+
+//Variables
+const Self_Args = process.argv.slice(2)
+
+//Main
+if(Self_Args.length == 0){
+    console.log(`node index.js <directory>
+Example: node index.js ./test`)
+    process.exit()
+}
+
+if(!Fs.existsSync(Self_Args[0])){
+    I2rys.log("yellowish", "CRITICAL", "SRF Debugger:", "Invalid directory path.")
+    I2rys.log("yellowish", "INFO", "SRF Debugger:", "Exiting...")
+    process.exit()
+}
+
+Secure_RM(`${Self_Args[0]}/*`).then(()=>{
+    I2rys.log("yellowish", "INFO", "SRF Debugger:", `${Self_Args[0]} files have been removed securely & safely.`)
+    I2rys.log("yellowish", "INFO", "SRF Debugger:", "Exiting...")
+    process.exit()
+}).catch(()=>{
+    I2rys.log("yellowish", "CRITICAL", "SRF Debugger:", `Unable to remove ${Self_Args[0]} files due to invalid path/low permission.`)
+    I2rys.log("yellowish", "INFO", "SRF Debugger:", "Exiting...")
+    process.exit()
+})
